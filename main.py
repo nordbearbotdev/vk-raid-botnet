627278c21f523b8f4809a4d7018f6f99fc93be28
Commit 404 <85753549+nordbearbotdev@users.noreply.github.com>
2022-08-29 16:22:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from rich.console import Console
 import sys
+import colorama
+
+from colorama import Fore, init
+
+from pystyle import Center, Colors, Colorate, System
 
 from settings.settings import MenuSettings
 from settings.settings_session import SessionsRead
@@ -9,25 +14,46 @@ console = Console()
 session_list = SessionsRead()
 list_function = MenuSettings()
 
-console.print('''[bold]
+banner = r'''
+
+⠄⠄⠄⠄⠄⠄⢴⣿⣿⣿⣷⣤⣤⣄⡀⢠⣤⣄⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠙⢿⣿⣟⣉⣠⣤⣤⣤⣉⡻⢿⣇⣠⣤⣤⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠄⠄⠹⣿⣿⣿⣿⣿⠟⢋⣁⣄⡈⠻⣿⢃⣴⠷⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⢀⡴⠶⢾⣿⣿⣿⣿⣿⠟⣩⣭⣭⣄⡀⠛⢡⣾⡏⠉⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⢸⡏⠄⠄⠢⠘⣿⣿⣿⠘⣿⣿⡿⠁⠙⣿⡄⢸⣿⡇⢰⡷⠄⠄
+⠄⠄⠄⠄⠄⢀⣼⣧⠄⠄⠄⢰⣿⣿⠋⣷⢸⣿⠇⢰⠆⣿⡇⠈⢿⡿⠆⠄⣠⡄
+⠄⠄⣀⣴⣾⣿⣿⣿⣆⠄⠄⣾⣿⣿⣇⢿⡌⠻⣤⣤⠶⠟⣁⠐⡖⠒⠂⢉⣩⣀
+⠄⣼⣿⣿⡿⠛⣥⣤⣤⡄⠄⠘⠋⠛⠿⡌⠛⠳⠶⠆⣠⣴⣿⣷⡘⢋⣽⣿⡿⠟
+⣼⣿⣿⣿⡇⢸⣇⡈⠟⢠⣄⣰⢏⡠⠄⠰⢶⣶⣶⣾⣿⣿⣿⣿⠿⠿⣟⣡⠴⠄
+⢿⣿⣿⣿⣧⠈⣿⣷⠄⢸⣿⣿⣯⣠⠞⣠⣈⠻⠿⠿⢿⣛⣉⠤⠶⠛⣫⡀⠄⠄
+⠈⣿⣿⣿⣿⣷⠙⢿⡄⣸⣿⣿⣿⣷⣾⠟⠁⠄⠄⠄⠄⠨⣻⡄⣀⣴⠏⠄⠄⠄
+⠄⠄⠙⢿⣿⣿⣿⡖⣸⣿⣿⣿⣿⠋⣠⣾⡿⠇⣀⡀⠄⠄⠁⣿⠁⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠈⠛⠿⢀⣿⣿⣿⣿⡇⣤⣤⣴⠶⠟⠛⠉⠙⠛⠛⠁⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⢠⣾⣿⣿⣿⣿⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠻⣿⣿⣿⡟⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
 GitHub: https://github.com/Madara225/vk-raid-botnet
 Author's channel: https://t.me/Pepe_devs and https://t.me/huis_bn
 test version
-''')
 
+'''
+
+System.Clear()
+print(Colorate.Horizontal(Colors.purple_to_blue, Center.XCenter(banner)))  
+SlowPrint(f"\n 
 
 def botnet_main():
 
     menu_function={}
 
-    console.print(f'[bold white]botnet accounts >> {len(session_list.sessions)}')
+    print(f'[bold white]botnet accounts >> {len(session_list.sessions)}')
     for num_function, function in enumerate(
             list_function.menu_botnet,
             start=1
         ):
 
         menu_function[num_function]=function
-        console.print(f'[bold white][{num_function}] {function.__doc__}')
+        print(f'[bold white][{num_function}] {function.__doc__}')
 
     try:
         menu = int(console.input('>> '))
@@ -37,7 +63,7 @@ def botnet_main():
                 classes(session_list.sessions)
 
     except KeyboardInterrupt:
-        console.print('\n[blue]<https://t.me/pepe_devs>[/]')
+        print('\n[blue]<https://t.me/pepe_devs>[/]')
         sys.exit()
 
     except:
